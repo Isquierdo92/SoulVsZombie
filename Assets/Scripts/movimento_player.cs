6653c7daa6251445f8e19d4cf11ea7477f17de86
HIqueRS <39134729+HIqueRS@users.noreply.github.com>
2018-12-01 18:44:33 -0200

    Move_Character_Control

--- Assets/Scripts/movimento_player.cs
+++ Assets/Scripts/movimento_player.cs
@@ -6,6 +6,7 @@ public class movimento_player : MonoBehaviour {
 
     public float velocidade;
     public Rigidbody rg;
+  
     public float jump;
     bool chao;
     public GameObject elevator;
@@ -13,20 +14,23 @@ public class movimento_player : MonoBehaviour {
     public bool recarregar;
     public bool chave;
 
+    public Vector3 movimento;
+    public float gravidade = 5;
+
 
     // Use this for initialization
     void Start () {
         //PEGA O RIGIDBODY DO PROPRIO OBJETO COMO REFERENCIA
-        rg = gameObject.GetComponent<Rigidbody>();
+        //rg = gameObject.GetComponent<Rigidbody>();
         //SETA O CHAO COMO TRUE, PARA TESTAR O PULO
         chao = true;
         recarregar = false;
 
         //SETAR O DRAG, QUANTO MAIOR O DRAG MAIOR A LENTIDÃO QUE O OBJETO RECEBE E NÃO DESLIZA
-        rg.GetComponent<Rigidbody>().drag = 1.5f;
+        //rg.GetComponent<Rigidbody>().drag = 1.5f;
 
         //SETA VELOCIDADE DE MOVIMENTO EM X
-        velocidade = 1000.0f;
+        velocidade = 7.0f;
         //FORCA PARA O PULO, TEM QUE SER ALTA
         jump = 1200.0f;
 
@@ -36,7 +40,19 @@ public class movimento_player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-       
+        if (GetComponent<CharacterController>().isGrounded)
+        {
+            movimento.y = 0;//gravidade * Time.deltaTime;
+            chao = true;
+        }
+        else
+        {
+            movimento.y -= gravidade/2 * Time.deltaTime/2;
+        }
+
+        movimento.x = Input.GetAxis("Horizontal") * velocidade * Time.deltaTime;
+        GetComponent<CharacterController>().Move(movimento);
+
 
         if (Input.GetKey("w"))
         {
@@ -44,7 +60,8 @@ public class movimento_player : MonoBehaviour {
             if (chao == true)
             {
                 //ADICIONA A FORÇA EM Y PARA PULO
-                rg.AddForce(0.0f, jump * Time.deltaTime, 0.0f, ForceMode.Impulse);
+                // rg.AddForce(0.0f, jump * Time.deltaTime, 0.0f, ForceMode.Impulse);
+                movimento.y += 0.32525f;
             }
             
         }
@@ -60,12 +77,12 @@ public class movimento_player : MonoBehaviour {
         //SE APERTOU "D" ELE ANDARÁ PARA A DIREITA
         if (Input.GetKey("d"))
         {
-            rg.AddForce(velocidade * Time.deltaTime, 0.0f, 0.0f, ForceMode.Force);
+           // rg.AddForce(velocidade * Time.deltaTime, 0.0f, 0.0f, ForceMode.Force);
         }
         //SE APERTOU A ANDA PARA A ESQUERDA
         if(Input.GetKey("a"))
         {
-            rg.AddForce(-velocidade * Time.deltaTime, 0.0f, 0.0f, ForceMode.Force);
+            //rg.AddForce(-velocidade * Time.deltaTime, 0.0f, 0.0f, ForceMode.Force);
         }
 
         if (teleporte)
