9274cb08c967c0009bacf1e7a2f456a7a4483fee
HIqueRS <39134729+HIqueRS@users.noreply.github.com>
2018-12-01 10:34:59 -0200

    Ghost_Get_all_pieces

--- Assets/Scripts/Enemy_follow.cs
+++ Assets/Scripts/Enemy_follow.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class Enemy_follow : MonoBehaviour {
 
     public GameObject pers; // game object do playes
-    public GameObject pie; // pedaço
 
-    enum status { seguiplay, seguipiece, comepiece, preso }; // enum q não to usando pq deu ruim sla
+
+    public GameObject pie; // pedaço
+    public GameObject pie1; // pedaço
+    public GameObject pie2; // pedaço
+    public GameObject pie3; // pedaço
+    public GameObject pie4; // pedaço
+    public GameObject pie5;
+
+   // enum status { seguiplay, seguipiece, comepiece, preso }; // enum q não to usando pq deu ruim sla
 
 
     public float vel = 1; // pra testar a velocidade do fantasma
@@ -15,10 +22,16 @@ public class Enemy_follow : MonoBehaviour {
 
     public float distancia_piece; // distancia para o pedaço
 
-    bool Comido = false;
+    bool Comido = false,test = true;
 
     float Tempo =0; // tempo de comer
     public float TempoComendo = 5;
+
+    public GameObject testar;
+
+    public GameObject[] pieces;// = new GameObject [5];
+
+    bool PodePega = true;//testa se pode pegar o cara da fila
 
     //3 status
     //seguindo player
@@ -26,43 +39,78 @@ public class Enemy_follow : MonoBehaviour {
     //consumindo pedaço
     //preso
 
+    int ke =0;
+
+    
+
     // Use this for initialization
     void Start () {
         //pers = GameObject.Find
         // estado = seguiplay;
-        estado = 0; // começa seguindo o player
-    }
-	
-	// Update is called once per frame
-	void Update () {
+        estado = 1; // começa seguindo o player
+                    // pie = null;
 
+        //só pra testar a fila
+
+        pieces = new GameObject[5];
+        pieces[0] = pie1;
+        pieces[1] = pie2;
+        pieces[2] = pie3;
+        pieces[3] = pie4;
+        pieces[4] = pie5;
+               
+    }
+
+
+
+    private void FixedUpdate()
+    {
         //pegar a distancia pro pedaço
         if (pie != null)
-        distancia_piece = Vector3.Distance(transform.position, pie.GetComponent<Transform>().position);
+        {
+            distancia_piece = Vector3.Distance(transform.position, pie.GetComponent<Transform>().position);//talvez aaui problema
+        }
+
+    }
+    // Update is called once per frame
+    void Update () {
+
+        
 
         Test();//tirar isso depois
 
+        TemPedaco();
+
+
         switch (estado)
         {
+
             //seguir player
             case 0:
                 {
-                   estado = FollowPlayer(estado);
+
+                    print("follow player");
+                    estado = FollowPlayer(estado);
                 }
                 break;
             //seguir pedaço
             case 1:
                 {
+                    print("follow piece");
                     estado = FollowPiece(estado,pie);
                 }
                 break;
             //comendo
             case 2:
                 {
+                    print("eat piece");
                     //if(!Comido) //pie.
-                    if(pie != null)
-                    estado = EatPiece(estado);
+                    if (pie != null)
+                    {
+                        FollowPiece(estado, pie);
+                    }
                 }
+
                 break;
             //morto
             case 3:
@@ -83,13 +131,15 @@ public class Enemy_follow : MonoBehaviour {
         //andar para frente
         transform.Translate(new Vector3(0, 0, vel * Time.deltaTime));
 
+
+
         return estado;
     }
 
     int FollowPiece(int estado,GameObject p)
     {
         //pie = p; descomentar isso depoooois
-
+       // pieces.Enqueue(p);
        
         if(pie != null)
         {
@@ -97,36 +147,50 @@ public class Enemy_follow : MonoBehaviour {
                                                      //zerar angulos nao desejados
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
             //andar para frente
-            transform.Translate(new Vector3(0, 0, vel * Time.deltaTime));
-        }
-       
+            //transform.Translate(new Vector3(0, 0, vel * Time.deltaTime));
+            transform.position = Vector3.MoveTowards(transform.position, pie.transform.position, vel * Time.deltaTime);
 
-        Comido = false;
+        }
+
+
+        // Comido = false;
 
         if (distancia_piece < 1)//tá perto comendo
         {
-            estado = 2;//foi comer
+            print("Distancia" + distancia_piece);
+            EatPiece(2);
         }
 
         return estado;
     }
 
-    int EatPiece(int estad)
+    void SetPieces(GameObject p,int tipo)
+    {
+        pieces[tipo] = p;
+    }
+
+    void EatPiece(int estad)
     {
         //Debug.Log("funfou caraioo");
         Tempo += Time.deltaTime;
-       
 
         if (Tempo > TempoComendo)
         {
-            estad = 0;
-            Comido = true;
-            if (pie != null)
-            Destroy(pie);
-           
-        }
+            print("Tempo: " + Tempo);
+            print("Tempo comendo: " + TempoComendo);
 
-        return estad;
+            estado = 0;
+
+            // Comido = true;
+            if (pie != null)
+            {
+                Destroy(pie);
+                PodePega = true;
+            }
+            Tempo = 0;
+
+        }
+        
     }
 
     int Dead(int estado)
@@ -148,11 +212,38 @@ public class Enemy_follow : MonoBehaviour {
     {
         if(Input.GetKey("a"))
         {
-            estado = 1;
+           
+            if(pieces[ke] != null)
+            {
+                pie = pieces[ke];
+            }
+            
+            ke++;
+
         }
         else if (Input.GetKey("b"))
         {
             estado = 0;
+        }
+    }
+
+    void TemPedaco()
+    {
+        if(PodePega)
+        {
+            for(int i =0;i <5; i++)
+            {
+                if (pieces[i] != null)
+                {
+                    //Debug.Log("oq diabos happing"+ i);
+                    pie = pieces[i];
+                    pie.transform.position = pieces[i].transform.position;
+                    PodePega = false;
+                    estado = 1;//perseguir pedaco
+                    break;
+                }
+            }
+           
         }
     }
 }
